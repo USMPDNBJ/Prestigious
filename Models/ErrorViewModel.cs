7f2cc77562802cd80fd387cbd23de3c00eea5beb
Death Gun24 <miguel_guzman6@usmp.pe>
2024-03-15 23:28:14 -0500

    primer commit

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace retoProgramacion1.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# RetodeProgramacion1
-Este repositorio se creo para poder completar el reto del 15/03/2024
