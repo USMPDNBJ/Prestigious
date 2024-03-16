703561f805b0dfcf2a31b56db28a8fcdb770e23c
Death Gun24 <miguel_guzman6@usmp.pe>
2024-03-15 23:32:57 -0500

    prueba gitignore1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RetodeProgramacion1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
