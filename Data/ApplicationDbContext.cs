ec3a765901156d39e35239e463c63f0bfea4a46f
Death Gun24 <miguel_guzman6@usmp.pe>
2024-03-16 10:45:12 -0500

    Segundo commit con--auth

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RetodeProgramacion1.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
