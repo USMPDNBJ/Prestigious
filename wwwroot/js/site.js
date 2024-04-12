9cdd9812de4a99ac80534aa8dc3b715c07603d2c
USMPDNBJ <david_nolasco@usmp.pe>
2024-04-12 02:41:16 -0500

    Fondo Cambiante

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var backgrounds = ['images/fondo2.png', 'images/fondo1.png','images/fondo3.png']; // Asegúrate de proporcionar las rutas correctas de tus imágenes
+
+var index = 0;
+function changeBackground() {
+  var container = document.getElementById('fondox');
+  container.style.backgroundImage = 'url(' + backgrounds[index] + ')';
+  index = (index + 1) % backgrounds.length; // Cambia al siguiente fondo de manera cíclica
+}
+
+setInterval(changeBackground, 7000);
