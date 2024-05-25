0e455cd43fd19c91aafd453ef084f3871dc6b0b0
USMPDNBJ <david_nolasco@usmp.pe>
2024-05-24 21:32:57 -0500

    "rest"

---
+++ Controllers/Rest/ProductoApiController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Prestigious.Data;
+using Prestigious.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Prestigious.Controllers.Rest
+{
+    [ApiController]
+    [Route("api/producto")]
+    public class ProductoApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductoApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<List<Producto>>> List()
+        {
+            var productos = await _context.DataProducto.ToListAsync();
+             if(productos == null)
+                return NotFound();
+            return Ok(productos);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Prestigious.Data;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,6 +29,15 @@ builder.Services.AddSession(options =>
     options.Cookie.IsEssential = true;
 });
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API",
+        Version = "v1",
+        Description = "Descripción de la API"
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,6 +51,12 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
